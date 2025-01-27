adef8a7eab432d90982fb6709a04242c7a56e88e
dreamprogrammin <mbabazov@list.ru>
2025-01-27 07:58:41 +0500

    settins config and install module

--- app.vue
+++ app.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <h1>Hello world</h1>
   </div>
 </template>
+<style lang="scss">
+h1 {
+  @include Title;
+  color: $primary;
+}
+</style>
